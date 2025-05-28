89d30e63bfb570e3d4d553d8b0f6c45bc7027f2f
aszostek <01187292@pw.edu.pl>
2025-05-28 22:32:38 +0200

    changed cors policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,10 +34,6 @@ class MessageRequest(BaseModel):
 
 
 origins = [
-    "http://localhost:5173",
-    "localhost:5173",
-    "https://inquiro-1-rai1.onrender.com",
-    "https://inquiro-lrza.onrender.com",
     "https://inquiro-two.vercel.app"
 ]
 
